1bbb7c89c98413af475dd3cc67b5af5f30ea5203
VictorTao1998 <jit124@ucsd.edu>
2021-10-31 20:13:35 -0700

    init

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 export PYTHONWARNINGS="ignore"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/PAM/finetune.py \
---savepath='/jianyu-fast-vol/PAM/PAM_train' \
+--savepath='/jianyu-fast-vol/eval/PAM_train' \
 --config-file '/jianyu-fast-vol/PAM/configs/remote_train_gan.yaml' \
 --print_freq 100 \
 --save_freq 1
